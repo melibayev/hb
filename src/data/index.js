dec1f4f48375dcd7ec8dd43162bb2f769d596124
melibayev <melibayefff1@gmail.com>
2024-11-05 20:57:38 +0500

    features added

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,7 @@
 import VIDEO1 from '../assests/videos/demo/1.mp4'
 import VIDEO2 from '../assests/videos/demo/2.mp4'
-// import VIDEO2 from '../assests/videos/3.mp4'
+import VIDEO3 from '../assests/videos/demo/3.mp4'
+import VIDEO4 from '../assests/videos/demo/4.mp4'
 export const banner = [
     {
         id: 1,
@@ -10,8 +11,12 @@ export const banner = [
         id: 2,
         video: VIDEO2
     },
-    // {
-    //     id: 3,
-    //     video: VIDEO3
-    // }
+    {
+        id: 3,
+        video: VIDEO3
+    },
+    {
+        id: 4,
+        video: VIDEO4
+    }
 ]
