1ce2554143bd1ed36f4b2156280a01a1017a99b2
melibayev <melibayefff1@gmail.com>
2024-11-25 03:00:14 +0500

    api connection

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+      },
+    },
+  },
+};
--- src/components/context/DataContext.jsx
+++ src/components/context/DataContext.jsx
@@ -6,28 +6,18 @@ const DataContext = createContext();
 export const useData = () => useContext(DataContext);
 
 export const DataProvider = ({ children }) => {
-    const token = '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';  
-    const repoOwner = 'melibayev'; 
-    const repoName = 'data';  
-    const filePath = 'db.json';
     const [products, setProducts] = useState(null);
     const [videos, setVideos] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    
 
     useEffect(() => {
-      axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`, {
-        headers: {
-          'Authorization': `token ${token}`
-        }
-      }).then((response) => {
+      axios.get("https://raw.githubusercontent.com/melibayev/data/main/db.json")
+      .then((response) => {
         
-        const fileContent = response.data.content;
-        const decodedContent = atob(fileContent); 
-        const jsonData = JSON.parse(decodedContent);
-        
-        setProducts(jsonData.products); 
-        setVideos(jsonData.videos); 
+        setProducts(response.data.products); 
+        setVideos(response.data.videos); 
         setLoading(false); 
       })
       .catch((err) => {
