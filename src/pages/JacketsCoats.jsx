913996001ab8a6410677305c5a9cb74362ad181f
melibayev <melibayefff1@gmail.com>
2025-01-04 13:51:47 +0500

    coats category update

--- src/pages/JacketsCoats.jsx
+++ src/pages/JacketsCoats.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useData } from "../components/context/DataContext";
 
 import styles from '../sass/pages/JacketsCoats.module.scss';
@@ -6,9 +6,15 @@ import WishlistProduct from "../components/product/WishlistProduct";
 
 const JacketsCoats = () => {
   const { products } = useData()
-  const coats = products?.filter(product => product.category.includes('coats')) || [];
+  const allCoats = products?.filter(product => product.category.includes('coats')) || [];
+  const [coats, setCoats] = useState(allCoats)
   const { videos } = useData()
   const video = videos?.find(video => video.id === "d97fa351-ed8a-4e46-b1a4-d00f67b2a03e")
+  
+  const coatsCategory = (key) => {
+    setCoats(products?.filter(product => product.category.includes(key)) || [])
+  }
+  
   if (!videos) {
     return <div>loading...</div>
   }
@@ -23,19 +29,19 @@ const JacketsCoats = () => {
       <section id={styles.products}>
         <div className="container">
           <div className={styles['products-category']}>
-            <div className={styles['products-category-item']}>
+            <div className={styles['products-category-item']} onClick={() => coatsCategory('formal-coats')}>
                 <div className={styles['products-category-item-img']}>
                   <img src="https://raw.githubusercontent.com/melibayev/data/main/home_images/hbna50529745_001_100.jpg" alt="" />
                 </div>
                 <p>Formal Coats</p>              
             </div>
-            <div className={styles['products-category-item']}>
+            <div className={styles['products-category-item']} onClick={() => coatsCategory('casual-coats')}>
                 <div className={styles['products-category-item-img']}>
                   <img src="https://raw.githubusercontent.com/melibayev/data/main/home_images/hbna50528439_404_100.jpg" alt="" />
                 </div>
                 <p>Casual Coats</p>              
             </div>
-            <div className={styles['products-category-item']}>
+            <div className={styles['products-category-item']} onClick={() => coatsCategory('parkas')}>
                 <div className={styles['products-category-item-img']}>
                   <img src="https://raw.githubusercontent.com/melibayev/data/main/home_images/hbna50525237_001_100.jpg" alt="" />
                 </div>
